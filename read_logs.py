a2e881bce7ed9073f8d663b975eb3d1807ae5cd6
qodirovmuhammadmuso3-hub <qodirovmuhammadmuso3@gmail.com>
2026-03-30 23:20:51 +0500

    Render-ga moslash: diagnostika fayllari o'chirib tashlandi

--- read_logs.py
+++
@@ -1,6 +0,0 @@
-with open("bot_error.log", "rb") as f:
-    content = f.read()
-    try:
-        print(content.decode("utf-16le"))
-    except:
-        print(content.decode("utf-8", errors="ignore"))
